b43237beac2d9e62bfa29b7b1d19aad78bf5f17a
Mohammad Meftahi <meftahee@gmail.com>
2025-02-22 15:41:21 +0330

    feat: connect socket in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import { WebApp } from "./pages/webapp";
 import { Client } from "./pages/client";
+import { socket } from "./socket";
 
 function App() {
+  useEffect(() => {
+    socket.connect();
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <section className="container mx-auto">
       <Routes>
-        <Route index element={<Client />} />
+        <Route path="client" element={<Client />} />
         <Route path="webapp" element={<WebApp />} />
       </Routes>
     </section>
